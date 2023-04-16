61a772a13bd090be1443c49eb94cfc71f62bdc91
Aylaimran-02 <104843244+Aylaimran-02@users.noreply.github.com>
2023-04-17 00:33:43 +0500

    react-info-website

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+
+import React from "react";
+
+function Main(){
+return(
+
+    <main>
+        <h1 className="main-title">Fun facts about react</h1>
+        <ul className="main-facts">
+            <li>was first released in 2013</li>
+            <li>was originally created by Jordan walke</li>
+            <li>Has well over 100k stars</li>
+            <li>It's maintained by facebook</li>
+        </ul>
+    </main>
+    
+)
+
+
+}
+
+
+export default Main;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+
+export default function Navbar(){
+
+return (
+
+    <nav>
+        <img className="nav-icon" src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="" />
+        <h3 className="nav-logo-text">React Facts</h3>
+        <h4 className="nav-title">React Course Project 1</h4>
+    </nav>
+)
+
+}
